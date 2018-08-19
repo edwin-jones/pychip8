4d9e9305469a4570b83d2c9710347650b9c3cb52
Edwin Jones <edwinjones@outlook.com>
2018-08-19 11:38:37 +0100

    test renames

--- pychip8/tests/operation_mapper_tests.py
+++ pychip8/tests/operation_mapper_tests.py
@@ -11,27 +11,30 @@ class TestOperationMapper(unittest.TestCase):
         self.opcode = Opcode(0xABCD)
         self.operation_mapper = OperationMapper()
 
-    def _test_execution(self, word, operation_type):
+    def _test_mapping(self, word, operation_type):
         operation = self.operation_mapper.find_operation(word)
         self.assertTrue(isinstance(operation, operation_type))
 
-    def test_operation_mapper_thows(self):
+    def test_thows_if_no_mapping(self):
         self.assertRaises(KeyError, self.operation_mapper.find_operation, 0x2FFF)
 
-    def test_operation_mapper_set_index_register(self):
-        self._test_execution(0xA123, SetIndexRegister)
+    def test_set_index_register_mapping(self):
+        self._test_mapping(0xA123, SetIndexRegister)
 
-    def test_operation_mapper_set_register(self):
-        self._test_execution(0x61CD, SetGeneralPurposeRegister)
+    def test_set_general_purpose_register_mapping(self):
+        self._test_mapping(0x61CD, SetGeneralPurposeRegister)
 
-    def test_operation_mapper_copy_register(self):
-        self._test_execution(0x8120, CopyGeneralPurposeRegister)
+    def test_copy_general_purpose_register_mapping(self):
+        self._test_mapping(0x8120, CopyGeneralPurposeRegister)
 
-    def test_operation_mapper_set_delay_timer(self):
-        self._test_execution(0xF615, SetDelayTimer)
+    def test_set_delay_timer_mapping(self):
+        self._test_mapping(0xF615, SetDelayTimer)
 
-    def test_operation_mapper_set_sound_timer(self):
-        self._test_execution(0xF618, SetSoundTimer)
+    def test_set_sound_timer_mapping(self):
+        self._test_mapping(0xF618, SetSoundTimer)
+    
+    def test_clear_display_mapping(self):
+        self._test_mapping(0x00E0, ClearDisplay)
 
 
 if __name__ == '__main__':
