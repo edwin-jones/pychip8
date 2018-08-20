7c8025ccc7f37c5756805da7e6b650e8b90e24cc
Edwin Jones <edwinjones@outlook.com>
2018-08-20 20:58:44 +0100

    make operations private

--- pychip8/operation_mapper.py
+++ pychip8/operation_mapper.py
@@ -6,21 +6,21 @@ from collections import OrderedDict
 class OperationMapper():
 
     def __init__(self):
-        self.operations = OrderedDict()
+        self._operations = OrderedDict()
 
-        self.operations[0x00E0] = ClearDisplay()
-        self.operations[0x1FFF] = Goto()
-        self.operations[0x3FFF] = SkipIfEqual()
-        self.operations[0x6FFF] = SetGeneralPurposeRegister()
-        self.operations[0x8FF0] = CopyGeneralPurposeRegister()
+        self._operations[0x00E0] = ClearDisplay()
+        self._operations[0x1FFF] = Goto()
+        self._operations[0x3FFF] = SkipIfEqual()
+        self._operations[0x6FFF] = SetGeneralPurposeRegister()
+        self._operations[0x8FF0] = CopyGeneralPurposeRegister()
 
-        self.operations[0xAFFF] = SetIndexRegister()
-        self.operations[0xFF15] = SetDelayTimer()
-        self.operations[0xFF18] = SetSoundTimer()
+        self._operations[0xAFFF] = SetIndexRegister()
+        self._operations[0xFF15] = SetDelayTimer()
+        self._operations[0xFF18] = SetSoundTimer()
 
     def find_operation(self, word):
 
-        for key in self.operations:
+        for key in self._operations:
 
             key_as_opcode = Opcode(key)
             word_as_opcode = Opcode(word)
@@ -30,8 +30,8 @@ class OperationMapper():
 
             mask = key | word
 
-            if mask in self.operations:
-                return self.operations[mask]
+            if mask in self._operations:
+                return self._operations[mask]
 
         raise KeyError(f"Opcode {word:#06x} not present in list of valid operations")
 
