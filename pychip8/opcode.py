62a1cd0f3c704332692d4130889b77a512e35f6d
Edwin Jones <edwinj85@gmail.com>
2018-08-21 22:30:47 +0100

    fix opcode bug

--- pychip8/opcode.py
+++ pychip8/opcode.py
@@ -16,6 +16,7 @@ class Opcode:
         # We use bitwise-and with a mask to extract specific nibbles.
         self.word = uint16(word)
         self.a = byte((word & 0xF000) >> 12) # we just want the most significant bits/nibble here so we bitshift right
+        self.nnn = uint16(word & 0x0FFF)
         self.nn = byte(word & 0x00FF)
         self.n = byte(word & 0x000F)
         self.x = byte((word & 0x0F00) >> 8) # Where don't use the lower nibbles, bitshift right to get just the raw value
