c8ce6a8aa42f676fee636cc5bcbb6b6ccc7d7b2b
Edwin Jones <edwinj85@gmail.com>
2018-08-23 22:33:19 +0100

    reorganise ops

--- pychip8/operations/__init__.py
+++ pychip8/operations/__init__.py
@@ -1,18 +1,23 @@
 from pychip8.operations.copy_general_purpose_register import CopyGeneralPurposeRegister
 from pychip8.operations.set_general_purpose_register import SetGeneralPurposeRegister
 from pychip8.operations.set_index_register import SetIndexRegister
-from pychip8.operations.set_sound_timer import SetSoundTimer
-from pychip8.operations.set_delay_timer import SetDelayTimer
-from pychip8.operations.clear_display import ClearDisplay
-from pychip8.operations.goto import Goto
-from pychip8.operations.skip_if_equal import SkipIfEqual
-from pychip8.operations.skip_if_not_equal import SkipIfNotEqual
-from pychip8.operations.skip_if_x_y_equal import SkipIfXyEqual
 from pychip8.operations.increment_general_purpose_register import IncrementGeneralPurposeRegister
-from pychip8.operations.bitwise_and import BitwiseAnd
-from pychip8.operations.bitwise_or import BitwiseOr
-from pychip8.operations.bitwise_xor import BitwiseXor
-from pychip8.operations.add_y_to_x import AddYToX
-from pychip8.operations.take_y_from_x import TakeYFromX
-from pychip8.operations.shifts.shift_x_left import ShiftXLeft
-from pychip8.operations.shifts.shift_x_right import ShiftXRight
+
+from pychip8.operations.graphics.clear_display import ClearDisplay
+
+from pychip8.operations.jumps.goto import Goto
+from pychip8.operations.jumps.skip_if_equal import SkipIfEqual
+from pychip8.operations.jumps.skip_if_not_equal import SkipIfNotEqual
+from pychip8.operations.jumps.skip_if_x_y_equal import SkipIfXyEqual
+
+from pychip8.operations.arithmetic.add_y_to_x import AddYToX
+from pychip8.operations.arithmetic.take_y_from_x import TakeYFromX
+
+from pychip8.operations.bitwise.shift_x_left import ShiftXLeft
+from pychip8.operations.bitwise.shift_x_right import ShiftXRight
+from pychip8.operations.bitwise.bitwise_and import BitwiseAnd
+from pychip8.operations.bitwise.bitwise_or import BitwiseOr
+from pychip8.operations.bitwise.bitwise_xor import BitwiseXor
+
+from pychip8.operations.timers.set_sound_timer import SetSoundTimer
+from pychip8.operations.timers.set_delay_timer import SetDelayTimer
