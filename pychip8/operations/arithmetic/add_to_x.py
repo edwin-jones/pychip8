c6839d9756246bdb337c3d2395457989df6876a7
Edwin Jones <edwinjones@outlook.com>
2018-08-29 00:47:42 +0100

    turn off overflow errors in opcode

--- pychip8/operations/arithmetic/add_to_x.py
+++ pychip8/operations/arithmetic/add_to_x.py
@@ -1,3 +1,7 @@
+from numpy import uint8 as byte
+import numpy
+
 class AddToX():
     def execute(self, opcode, cpu):
-        cpu.general_purpose_registers[opcode.x] += opcode.nn
+        with numpy.errstate(over='ignore'):
+            cpu.general_purpose_registers[opcode.x] += byte(opcode.nn)
