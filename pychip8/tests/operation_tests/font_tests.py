b1fe3d9b78fe819fc642171d651c8f464a8170d0
Edwin Jones <edwinj85@gmail.com>
2018-08-28 19:52:16 +0100

    font test suite

--- pychip8/tests/operation_tests/font_tests.py
+++ pychip8/tests/operation_tests/font_tests.py
@@ -1,23 +1,16 @@
 from pychip8.opcode import Opcode
 from pychip8.tests.operation_tests.operation_test_case import OperationTestCase
 from pychip8.cpu import Cpu
-from pychip8.operations import *
+from pychip8.operations import LoadCharacterAddress
 
-from numpy import uint8 as byte
 import pychip8.font as Font
  
 class FontTests(OperationTestCase):
     
-    def setUp(self):
-        super().setUp()
-        self.operation = LoadCharacterAddress()
-
-    def _test_load_character_address(self, word):
-        opcode = Opcode(word)
-        self.operation.execute(opcode, self.cpu)
-
-    def test_load_character_address_a(self):
-        self._test_load_character_address(0xFA29)
-        self.assertEqual(self.cpu.index_register, 0xA * Font.CHAR_SIZE_IN_BYTES)
-
+    def test_load_character_addresses(self):
+        operation = LoadCharacterAddress()
+        for number in range(0xF + 1):
+            opcode = Opcode(0xF029 | (number << 8))
+            operation.execute(opcode, self.cpu)
+            self.assertEqual(self.cpu.index_register, number * Font.CHAR_SIZE_IN_BYTES)
 
