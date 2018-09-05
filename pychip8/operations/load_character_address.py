c3e1bb72013441efe2290a867b4219d55386eb82
Edwin Jones <edwin.jones@mediatonic.co.uk>
2018-09-05 15:32:06 +0100

    fix font load instruction and test

--- pychip8/operations/load_character_address.py
+++ pychip8/operations/load_character_address.py
@@ -1,7 +1,7 @@
-from numpy import uint8 as byte
+from numpy import uint16
 
 import pychip8.font as Font
 
 class LoadCharacterAddress():
     def execute(self, opcode, cpu):
-        cpu.index_register = byte(opcode.x * Font.CHAR_SIZE_IN_BYTES)
+        cpu.index_register = uint16(cpu.general_purpose_registers[opcode.x] * Font.CHAR_SIZE_IN_BYTES)
--- pychip8/tests/operation_tests/font_tests.py
+++ pychip8/tests/operation_tests/font_tests.py
@@ -3,14 +3,17 @@ from pychip8.tests.operation_tests.operation_test_case import OperationTestCase
 from pychip8.cpu import Cpu
 from pychip8.operations import LoadCharacterAddress
 
+from numpy import uint8 as byte
+
 import pychip8.font as Font
  
 class FontTests(OperationTestCase):
     
     def test_load_character_addresses(self):
         operation = LoadCharacterAddress()
-        for number in range(0xF + 1):
-            opcode = Opcode(0xF029 | (number << 8))
+        for number in range(16):
+            opcode = Opcode(0xF029)
+            self.cpu.general_purpose_registers[opcode.x] = number
             operation.execute(opcode, self.cpu)
             self.assertEqual(self.cpu.index_register, number * Font.CHAR_SIZE_IN_BYTES)
 
