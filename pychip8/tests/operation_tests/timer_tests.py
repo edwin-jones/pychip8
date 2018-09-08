3c01550622f3b8687ae63bfe1558799e93e13a8b
Edwin Jones <edwinj85@gmail.com>
2018-09-08 18:21:06 +0100

    update timer tests

--- pychip8/tests/operation_tests/timer_tests.py
+++ pychip8/tests/operation_tests/timer_tests.py
@@ -5,14 +5,16 @@ from pychip8.operations import *
 
 from numpy import uint8 as byte
 
- 
+
 class TimerTests(OperationTestCase):
 
     def test_set_delay_timer(self):
-        self._test_cpu_attribute_equals_value_after_execution(0xF615, SetDelayTimer(), 'delay_timer', 0x6)
-    
+        self.cpu.general_purpose_registers[6] = 0x8
+        self._test_cpu_attribute_equals_value_after_execution(0xF615, SetDelayTimer(), 'delay_timer', 0x8)
+
     def test_set_sound_timer(self):
-        self._test_cpu_attribute_equals_value_after_execution(0xF618, SetSoundTimer(), 'sound_timer', 0x6)
+        self.cpu.general_purpose_registers[6] = 0x7
+        self._test_cpu_attribute_equals_value_after_execution(0xF618, SetSoundTimer(), 'sound_timer', 0x7)
 
     def test_set_x_to_delay_timer(self):
         opcode = Opcode(0xF207)
@@ -20,3 +22,4 @@ class TimerTests(OperationTestCase):
         self.cpu.delay_timer = byte(0xA)
         operation.execute(opcode, self.cpu)
         self.assertEqual(self.cpu.general_purpose_registers[opcode.x], self.cpu.delay_timer)
+
